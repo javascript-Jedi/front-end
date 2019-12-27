16898a45d2f6ef4db2d12c6ea866276bf7fcb5c8
Anton DeXy <podolyananton@gmail.com>
2019-12-27 18:24:51 +0200

    initial

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav'
+import '../static/style.scss'
+
+const Home = () => (
+  <main>
+    
+  </main>
+)
+
+export default Home
